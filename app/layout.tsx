7a1f1e37077dd291204566b5bf14491c083bd5bf
roblesdotdev <httpsnip@gmail.com>
2023-07-01 16:01:37 -0300

    lint & format config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Project Boost",
+  title: 'Project Boost',
   description: `"Project-Boost" es una plataforma en línea que conecta a profesionales 
 voluntarios con proyectos disponibles en ONGs y startups que requieren colaboración`,
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-2xl">Working...</h1>
+      <h1 className="text-2xl font-bold">Working...</h1>
     </main>
-  );
+  )
 }
